4f9353edf3144152d5bafd9766b85c969de362f3
lawrencegorris <gorrislawrence.personal@gmail.com>
2021-07-19 16:23:47 +0200

    Added custom module called People with some data

---
+++ People.js
@@ -0,0 +1,18 @@
+const people = [
+    {
+        id: 1,
+        name: 'Eddy Wally'
+    },
+
+    {
+        id: 2,
+        name: 'Pinocchio Woodboy'
+    },
+
+    {
+        id: 3,
+        name: 'Superman'
+    }
+]
+
+module.exports = people
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const app = express()
-const port = 9001
+const port = process.env.port || 9001
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+const people = require('./People')
+
+app.use(express.static('public'))
+
+app.get('/people', (req, res) => {
+    res.status(200).send(people)
+})
 
 app.get('/some-data', (req, res) => {
-    res.send('You can post some data here')
-  })
+    res.send('You can post some data here :)')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-  })
+})
